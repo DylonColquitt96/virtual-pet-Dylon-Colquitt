b7819d34c418c822a96a7e6b1319c578768f32ba
Dylon-Colquitt <dragon12907@gmail.com>
2020-12-03 15:00:44 -0500

    Thursday 2nd Update

--- src/main/java/virtualPet.java
+++ src/main/java/virtualPet.java
@@ -3,7 +3,7 @@ public class virtualPet {
    int hungerLevel;
    int thirstLevel;
    boolean hungry;
-   int maxCount =20;
+   int maxCount = 20;
 
 
     public virtualPet(){
@@ -17,16 +17,28 @@ public class virtualPet {
     }
 
   public void tickCounter( ){
+          setHungerLevel(hungerLevel -1);
+          //This set method if found on line 36 on virtualPet class.
+          setThirstLevel(thirstLevel -1);
+          //This set method if found on line 67 on virtualPet class.
+          setBoredomLevel(boredomLevel -1);
+          //uses a "set method" where the value can not exceed "maxCounter" but will go down below the "maxCounter"
+          //This set method if found on line 81 on virtualPet class.
 
-          boredomLevel -=1;
-          thirstLevel -=1;
-          hungerLevel -=1;
           System.out.println("Boredom Level: " + boredomLevel + " Hunger Level: " + hungerLevel +
                   " Thirst Level: " + thirstLevel);
       //Tick counter, is telling the levels to go down by 1 each time it loops back over to it thanks to the
       //while loop in the main application
       //Print line shows what each level is at the time for the user
    }
+
+
+  public void setHungerLevel(int newValue){
+        hungerLevel = newValue;
+        if ( hungerLevel > maxCount){
+            hungerLevel = maxCount;
+        }
+  }
 
 
     public boolean getHungerStatus() {
@@ -45,7 +57,6 @@ public class virtualPet {
         } else if (choice == 3) {
             hungerLevel += 1;
         }
-
         //we use the eat function on line 54 in the main application, shown as this "myVirtualPet.eat(foodChoice); "
         //we call our myVirtualPet which connects to this class, then we use .eat which connects to the method above.
         //with in the parameters (), we place food choice, which is what we named the input in which the user will type
@@ -53,11 +64,26 @@ public class virtualPet {
         //to food choice.
     }
 
+    public void setThirstLevel(int newValue){
+        thirstLevel = newValue;
+
+        if(thirstLevel > maxCount){
+            thirstLevel = maxCount;
+        }
+    }
+
     public void drink(int choice){
         if (choice == 1){
             thirstLevel +=3;
         } else if (choice == 2){
             thirstLevel +=2;
+        }
+    }
+
+    public void setBoredomLevel(int newValue){
+        boredomLevel = newValue;
+        if (boredomLevel > maxCount){
+            boredomLevel = maxCount;
         }
     }
 
