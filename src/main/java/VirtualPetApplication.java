6dd624b252751cc78d82671c0259b1d2aeb06e2f
Dylon-Colquitt <dragon12907@gmail.com>
2020-12-03 14:20:56 -0500

    Thursday Update

--- src/main/java/VirtualPetApplication.java
+++ src/main/java/VirtualPetApplication.java
@@ -3,10 +3,123 @@ import java.util.Scanner;
 public class VirtualPetApplication {
     public static void main(String[] args) {
         virtualPet myVirtualPet = new virtualPet();
+
         Scanner input = new Scanner(System.in);
         String petName = "";
         System.out.println("What will you name your pet?");
         petName = input.nextLine();
-        System.out.println("Hello, " + petName + "!");
+        System.out.println("Congratulations! You have adopted a dragon named " + petName + "!");
+        System.out.println("\n");
+
+        System.out.println("                      / \\\\  / \\                \n" +
+                "                     /.  \\\\/   \\      |\\___/|  \n" +
+                "  *----*           / / |  \\\\    \\  __/  O  O\\  \n" +
+                "  |   /          /  /  |   \\\\    \\_\\/  \\     \\    \n" +
+                " / /\\/         /   /   |    \\\\   _\\/    '@___@     \n" +
+                "/  /         /    /    |     \\\\ _\\/       |U\n" +
+                "|  |       /     /     |      \\\\\\/        |\n" +
+                "\\  |     /_     /      |       \\\\  )   \\ _|_\n" +
+                "\\   \\       ~-./_ _    |    .- ; (  \\_ _ _,\\'\n" +
+                "~    ~.           .-~-.|.-*      _        {-,\n" +
+                " \\      ~-. _ .-~                 \\      /\\'\n" +
+                "  \\                   }            {   .*\n" +
+                "   ~.                 '-/        /.-~----.\n" +
+                "     ~- _             /        >..----.\\\\\\\n" +
+                "         ~ - - - - ^}_ _ _ _ _ _ _.-\\\\\\");
+        int choice = 0;
+
+        while (choice != 5) {
+
+
+            //Tried to post up above the level of hunger and everything, but
+            //it didn't come out right
+            System.out.println("What would you like to do with " + petName + "?");
+            System.out.println("1. Feed " + petName);
+            System.out.println("2. Tell " + petName + " a story");
+            System.out.println("3. Let " + petName + " fly around");
+            System.out.println("4. Ask " + petName + " to show off");
+            System.out.println("5. Do nothing");
+
+            choice = input.nextInt();
+            if (choice == 1) {
+                do {
+                    System.out.println("What will you feed " + petName + "?");
+                    System.out.println("1. A pesky goblin");
+                    System.out.println("2. A cream cheese danish");
+                    System.out.println("3. A sprig of parsley");
+                    System.out.println("4. Nothing");
+                    int foodChoice = input.nextInt();
+
+                    myVirtualPet.eat(foodChoice);
+                    //links up to eat method in virtual pet line 40
+
+                    if (foodChoice == 1) {
+                        System.out.println(petName + " says, My favorite!\n");
+                        System.out.println("                      / \\\\  / \\                \n" +
+                                "                     /.  \\\\/   \\      |\\___/|  \n" +
+                                "  *----*           / / |  \\\\    \\  __/  ♡  ♡\\  \n" +
+                                "  |   /          /  /  |   \\\\    \\_\\/  \\     \\    \n" +
+                                " / /\\/         /   /   |    \\\\   _\\/    '@___@     \n" +
+                                "/  /         /    /    |     \\\\ _\\/       |U\n" +
+                                "|  |       /     /     |      \\\\\\/        |\n" +
+                                "\\  |     /_     /      |       \\\\  )   \\ _|_\n" +
+                                "\\   \\       ~-./_ _    |    .- ; (  \\_ _ _,\\'\n" +
+                                "~    ~.           .-~-.|.-*      _        {-,\n" +
+                                " \\      ~-. _ .-~                 \\      /\\'\n" +
+                                "  \\                   }            {   .*\n" +
+                                "   ~.                 '-/        /.-~----.\n" +
+                                "     ~- _             /        >..----.\\\\\\\n" +
+                                "         ~ - - - - ^}_ _ _ _ _ _ _.-\\\\\\");
+                    } else if (foodChoice == 2) {
+                        System.out.println(petName + " says, Mmmmmmmm\n");
+                        System.out.println("                      / \\\\  / \\                \n" +
+                                "                     /.  \\\\/   \\      |\\___/|  \n" +
+                                "  *----*           / / |  \\\\    \\  __/  O  O\\  \n" +
+                                "  |   /          /  /  |   \\\\    \\_\\/  \\     \\    \n" +
+                                " / /\\/         /   /   |    \\\\   _\\/    '@___@     \n" +
+                                "/  /         /    /    |     \\\\ _\\/       |U\n" +
+                                "|  |       /     /     |      \\\\\\/        |\n" +
+                                "\\  |     /_     /      |       \\\\  )   \\ _|_\n" +
+                                "\\   \\       ~-./_ _    |    .- ; (  \\_ _ _,\\'\n" +
+                                "~    ~.           .-~-.|.-*      _        {-,\n" +
+                                " \\      ~-. _ .-~                 \\      /\\'\n" +
+                                "  \\                   }            {   .*\n" +
+                                "   ~.                 '-/        /.-~----.\n" +
+                                "     ~- _             /        >..----.\\\\\\\n" +
+                                "         ~ - - - - ^}_ _ _ _ _ _ _.-\\\\\\");
+
+                    } else if (foodChoice == 3) {
+                        System.out.println(petName + " says, \"Thanks, I guess\" \n");
+                        System.out.println("                      / \\\\  / \\                \n" +
+                                "                     /.  \\\\/   \\      |\\___/|  \n" +
+                                "  *----*           / / |  \\\\    \\  __/  -  -\\  \n" +
+                                "  |   /          /  /  |   \\\\    \\_\\/  \\     \\    \n" +
+                                " / /\\/         /   /   |    \\\\   _\\/    '@___@     \n" +
+                                "/  /         /    /    |     \\\\ _\\/       |U\n" +
+                                "|  |       /     /     |      \\\\\\/        |\n" +
+                                "\\  |     /_     /      |       \\\\  )   \\ _|_\n" +
+                                "\\   \\       ~-./_ _    |    .- ; (  \\_ _ _,\\'\n" +
+                                "~    ~.           .-~-.|.-*      _        {-,\n" +
+                                " \\      ~-. _ .-~                 \\      /\\'\n" +
+                                "  \\                   }            {   .*\n" +
+                                "   ~.                 '-/        /.-~----.\n" +
+                                "     ~- _             /        >..----.\\\\\\\n" +
+                                "         ~ - - - - ^}_ _ _ _ _ _ _.-\\\\\\");
+                    } else if (foodChoice == 4) {
+                        break;
+                    }
+                }
+                while (!myVirtualPet.getHungerStatus());
+/*            } else if (choice == 2) {
+                    int boredomLevel = 0;
+                    while (boredomLevel)
+            } else if (choice == 3) {
+            } else if (choice == 4) {
+                }*/
+
+            }
+            myVirtualPet.tickCounter();
+        }
     }
+
 }
--- src/main/java/virtualPet.java
+++ src/main/java/virtualPet.java
@@ -1,20 +1,74 @@
 public class virtualPet {
-    
-    public int hungerStatus(int isHungry) {
-        return isHungry;
+   int boredomLevel;
+   int hungerLevel;
+   int thirstLevel;
+   boolean hungry;
+   int maxCount =20;
+
+
+    public virtualPet(){
+        boredomLevel = maxCount;
+        thirstLevel = maxCount;
+        hungerLevel = maxCount;
+
+
+    //Lists out the max of the levels of each field
+
     }
 
-    String name;
+  public void tickCounter( ){
 
-    public String nameVirtualPet(String i) {
-        name = i;
-        return name;
+          boredomLevel -=1;
+          thirstLevel -=1;
+          hungerLevel -=1;
+          System.out.println("Boredom Level: " + boredomLevel + " Hunger Level: " + hungerLevel +
+                  " Thirst Level: " + thirstLevel);
+      //Tick counter, is telling the levels to go down by 1 each time it loops back over to it thanks to the
+      //while loop in the main application
+      //Print line shows what each level is at the time for the user
+   }
+
+
+    public boolean getHungerStatus() {
+        if (hungerLevel <= 10) {
+            return hungry;
+        }
+        else
+            return !hungry;
     }
 
-    int boredomGauge;
+    public void eat(int choice) {
+        if (choice == 1){
+            hungerLevel += 3;
+        } else if(choice == 2){
+            hungerLevel += 2;
+        } else if (choice == 3) {
+            hungerLevel += 1;
+        }
 
-    public int excitedBored(int j) {
-        boredomGauge = j;
-        return boredomGauge;
+        //we use the eat function on line 54 in the main application, shown as this "myVirtualPet.eat(foodChoice); "
+        //we call our myVirtualPet which connects to this class, then we use .eat which connects to the method above.
+        //with in the parameters (), we place food choice, which is what we named the input in which the user will type
+        //out. And depending on what number they chose, then the above method will add the appropriate amount
+        //to food choice.
     }
+
+    public void drink(int choice){
+        if (choice == 1){
+            thirstLevel +=3;
+        } else if (choice == 2){
+            thirstLevel +=2;
+        }
+    }
+
+    public void play (int choice){
+        if (choice == 1){
+            boredomLevel += 3;
+        } else if (choice == 2){
+            boredomLevel += 2;
+        } else if ( choice == 3){
+            boredomLevel += 1;
+        }
+    }
+
 }
